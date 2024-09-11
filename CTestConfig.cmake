9b719b841aba2cf3b1aafdacac3d7ca61e9b3328
Daniel Pfeifer <daniel@pfeifer-mail.de>
2024-09-11 15:06:39 +0200

    Revert "Reapply "update cdash submit url""
    This reverts commit 1d0a8ee60dc947e3b1a01b36e1d451b2ea81a8e8.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,9 +7,9 @@
 set(CTEST_PROJECT_NAME "Example")
 set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "localhost:8080")
-set(CTEST_DROP_LOCATION "?project=purpleKarrot/Example")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "ci.purplekarrot.net")
+set(CTEST_DROP_LOCATION "/api/submit?project=purpleKarrot/Example")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_LABELS_FOR_SUBPROJECTS
